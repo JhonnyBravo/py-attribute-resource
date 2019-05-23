1c7ba73e2e61958024b9aaf6aa38f27dfb7a6694
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-05-23 20:45:11 +0900

    modified: test/test_owner_resource.py
    skipif を導入。
    Windows 用テストを追加。

--- test/test_owner_resource.py
+++ test/test_owner_resource.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import ConfigParser
+import platform
 import unittest
 
 from py_attribute_resource.py_owner_resource import OwnerResource
@@ -24,6 +25,7 @@ class TestFileOwner(unittest.TestCase):
     def tearDown(self):
         self.fr.delete()
 
+    @unittest.skipIf(platform.system() == "Windows", "Not supported on Windows.")
     def test1(self):
         """
         * Can change file owner.
@@ -33,13 +35,13 @@ class TestFileOwner(unittest.TestCase):
         * Can get current file owner.
         * Exit code equals 2.
         """
-        self.resource.set_owner(self.owner)
+        self.resource.set_attribute(self.owner)
         self.assertEqual(2, self.resource.code)
 
-        self.resource.set_owner(self.owner)
+        self.resource.set_attribute(self.owner)
         self.assertEqual(0, self.resource.code)
 
-        cur_owner = self.resource.get_owner()
+        cur_owner = self.resource.get_attribute()
         self.assertEqual(2, self.resource.code)
         self.assertEqual(self.owner, cur_owner)
 
@@ -57,6 +59,7 @@ class TestDirectoryOwner(unittest.TestCase):
     def tearDown(self):
         self.dr.delete()
 
+    @unittest.skipIf(platform.system() == "Windows", "Not supported on Windows.")
     def test1(self):
         """
         * Can change directory owner.
@@ -66,15 +69,50 @@ class TestDirectoryOwner(unittest.TestCase):
         * Can get current directory owner.
         * Exit code equals 2.
         """
-        self.resource.set_owner(self.owner)
+        self.resource.set_attribute(self.owner)
         self.assertEqual(2, self.resource.code)
 
-        self.resource.set_owner(self.owner)
+        self.resource.set_attribute(self.owner)
         self.assertEqual(0, self.resource.code)
 
-        cur_owner = self.resource.get_owner()
+        cur_owner = self.resource.get_attribute()
         self.assertEqual(2, self.resource.code)
         self.assertEqual(self.owner, cur_owner)
+
+
+class TestWindows(unittest.TestCase):
+    """
+    Tests for Windows platform.
+    """
+
+    def setUp(self):
+        config = ConfigParser.RawConfigParser()
+        config.read("test/resources/test.config")
+        self.owner = config.get("OwnerResource", "Owner")
+
+        self.resource = OwnerResource("test.txt")
+        self.fr = FileResource("test.txt")
+        self.fr.create()
+
+    def tearDown(self):
+        self.fr.delete()
+
+    @unittest.skipIf(platform.system() != "Windows", "Test for Windows.")
+    def test1(self):
+        """
+        Exit code equals 1 if platform equals Windows.
+        """
+        owner = self.resource.get_attribute()
+        self.assertEqual(1, self.resource.code)
+        self.assertFalse(owner)
+
+    @unittest.skipIf(platform.system() != "Windows", "Test for Windows.")
+    def test2(self):
+        """
+        Exit code equals 1 if platform equals Windows.
+        """
+        self.resource.set_attribute(self.owner)
+        self.assertEqual(1, self.resource.code)
 
 
 if __name__ == "__main__":
